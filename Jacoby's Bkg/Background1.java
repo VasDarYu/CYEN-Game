e24fff82995bbab0d561ce8214f6bbcb674c4e2a
JClark9701 <JClark9701@gmail.com>
2016-04-15 14:04:21 -0500

    Added files via upload

---
+++ Jacoby's Bkg/Background1.java
@@ -0,0 +1,26 @@
+package com.videogame.main;
+
+import javax.swing.*;
+
+class Window
+{
+	private static JFrame frame;
+	
+	public static void main(String [] args)
+	{
+		makeBackground1();
+	}
+	
+	public static void makeBackground1()
+	{
+		frame = new JFrame("Background 1 Test");
+								
+		JLabel background1 = new JLabel(new ImageIcon("C:\\Users\\JClark9701\\Pictures\\Background 1.png"));
+		frame.add(background1);
+				
+		frame.setSize(750, 525);
+
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
