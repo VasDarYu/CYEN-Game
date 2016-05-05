4cb2d5af8c4b0a3e6ab4f573ef33f95a3107e5aa
ToxicWaste97 <jimwallace97@gmail.com>
2016-05-04 22:57:58 -0500

    Changed Timer
    There was a slight error with the timer so that it would start counting
    at the launch of the game instead of the start of the level. This has
    been fixed.

--- Game.java
+++ Game.java
@@ -136,7 +136,6 @@ class Game extends Canvas implements Runnable
             if (System.currentTimeMillis() - timer > 1000)
             {
                 timer += 1000;
-                time += 1;
                 System.out.println("FPS: " + frames);
                 frames = 0;
             }
@@ -179,6 +178,7 @@ class Game extends Canvas implements Runnable
                 gameOver();
             }
             bullet();
+            time+=1;
         }
         //else
         //{
